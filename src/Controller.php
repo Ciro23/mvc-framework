fcb18a7301b6acd8d73990533246c6cc5f8c4003
Ciro23 <ciro.sorre23@gmail.com>
2021-01-15 16:37:27 +0100

    added comments on Controller static properties

--- src/Controller.php
+++ src/Controller.php
@@ -4,17 +4,28 @@ namespace Mvc;
 
 class Controller {
 
+    /**
+     * @var string $modelSuffix, common suffix for the model classes
+     */
     public static $modelSuffix = "Model";
+
+    /**
+     * @var string $viewsPath, common path where views are stored 
+     */
     public static $viewsPath = "/app/views/";
+
+    /**
+     * @var string $viewsSuffix, common suffix for the models file name
+     */
     public static $viewsSuffix = ".view.php";
 
     /**
-    * return a new model object
-    *
-    * @param string $model, the model name
-    *
-    * @return object $model
-    */
+     * return a new model object
+     *
+     * @param string $model, the model name
+     *
+     * @return object $model
+     */
     protected function model($model) {
         $model .= Controller::$modelSuffix;
         $model = ucfirst($model);
@@ -22,11 +33,11 @@ class Controller {
     }
 
     /**
-    * requires the wanted view
-    *
-    * @param string $view, the view name
-    * @param array $data, the data to pass to the view
-    */
+     * requires the wanted view
+     *
+     * @param string $view, the view name
+     * @param array $data, the data to pass to the view
+     */
     protected function view($view, $data = []) {
         if ($data === null) {
             // renders the pagenotfound view
